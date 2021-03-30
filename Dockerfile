9e99b65d52ecb5ffed410c68b938cd5fb428f0a1
Andreas Roth <andreas.roth@fastprotect.net>
2021-03-30 18:09:48 +0200

    allow to specify interfaces via env variable IFACE put config files into directory /config different config file for DHCPv6

--- Dockerfile
+++ Dockerfile
@@ -2,18 +2,16 @@ FROM ubuntu:focal
 
 MAINTAINER Robin Smidsrød <robin@smidsrod.no>
 
-ARG DEBIAN_FRONTEND=noninteractive
-
 RUN apt-get -q -y update \
- && DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends --no-install-suggests -y -o "DPkg::Options::=--force-confold" -o "DPkg::Options::=--force-confdef" dumb-init isc-dhcp-server \
+ && DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends --no-install-suggests -y -o "DPkg::Options::=--force-confold" -o "DPkg::Options::=--force-confdef" isc-dhcp-server \
  && apt-get -q -y autoremove \
  && apt-get -q -y clean \
  && cp -a /etc/dhcp /data \
  && rm -rf /var/lib/apt/lists/*
 
-ENV DHCPD_PROTOCOL=4
+ENV DHCPD_PROTOCOL=4 IFACE=
 
-VOLUME /data
+VOLUME /data /config
 
 COPY util/entrypoint.sh /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
--- util/entrypoint.sh
+++ util/entrypoint.sh
@@ -2,47 +2,50 @@
 
 set -e
 
-
-# Support docker run --init parameter which obsoletes the use of dumb-init,
-# but support dumb-init for those that still use it without --init
-if [ -x "/dev/init" ]; then
-    run="exec"
-else
-    run="exec /usr/bin/dumb-init --"
-fi
-
-# Single argument to command line is interface name
-if [ $# -eq 1 -a -n "$1" ]; then
-    # skip wait-for-interface behavior if found in path
-    if ! which "$1" >/dev/null; then
-        # loop until interface is found, or we give up
-        NEXT_WAIT_TIME=1
-        until [ -e "/sys/class/net/$1" ] || [ $NEXT_WAIT_TIME -eq 4 ]; do
-            sleep $(( NEXT_WAIT_TIME++ ))
-            echo "Waiting for interface '$1' to become available... ${NEXT_WAIT_TIME}"
-        done
-        if [ -e "/sys/class/net/$1" ]; then
-            IFACE="$1"
+if [ -z "$IFACE" ]; then
+    # Single argument to command line is interface name
+    if [ $# -eq 1 -a -n "$1" ]; then
+        # skip wait-for-interface behavior if found in path
+        if ! which "$1" >/dev/null; then
+            # loop until interface is found, or we give up
+            NEXT_WAIT_TIME=1
+            until [ -e "/sys/class/net/$1" ] || [ $NEXT_WAIT_TIME -eq 4 ]; do
+                sleep $(( NEXT_WAIT_TIME++ ))
+                echo "Waiting for interface '$1' to become available... ${NEXT_WAIT_TIME}"
+            done
+            if [ -e "/sys/class/net/$1" ]; then
+                IFACE="$1"
+            fi
         fi
     fi
-fi
 
-# No arguments mean all interfaces
-if [ -z "$1" ]; then
-    IFACE=" "
+    # No arguments mean all interfaces
+    if [ -z "$1" ]; then
+        IFACE=" "
+    fi
 fi
 
 if [ -n "$IFACE" ]; then
     # Run dhcpd for specified interface or all interfaces
 
+    config_dir="/config"
     data_dir="/data"
     if [ ! -d "$data_dir" ]; then
         echo "Please ensure '$data_dir' folder is available."
         echo 'If you just want to keep your configuration in "data/", add -v "$(pwd)/data:/data" to the docker run command line.'
         exit 1
     fi
+    if [ ! -d "$config_dir" ]; then
+        echo "Please ensure '$config_dir' folder is available."
+        echo 'If you just want to keep your configuration in "config/", add -v "$(pwd)/config:/config" to the docker run command line.'
+        exit 1
+    fi
 
-    dhcpd_conf="$data_dir/dhcpd.conf"
+    if [ "$DHCPD_PROTOCOL" == '6' ]; then
+        dhcpd_conf="$config_dir/dhcpd6.conf"
+    else
+        dhcpd_conf="$config_dir/dhcpd.conf"
+    fi
     if [ ! -r "$dhcpd_conf" ]; then
         echo "Please ensure '$dhcpd_conf' exists and is readable."
         echo "Run the container with arguments 'man dhcpd.conf' if you need help with creating the configuration."
@@ -68,9 +71,9 @@ if [ -n "$IFACE" ]; then
     if perl -e '($id,$name)=@ARGV;$short=substr $id,0,length $name;exit 1 if $name ne $short;exit 0' $container_id $HOSTNAME; then
         echo "You must add the 'docker run' option '--net=host' if you want to provide DHCP service to the host network."
     fi
-
-    $run /usr/sbin/dhcpd -$DHCPD_PROTOCOL -f -d --no-pid -cf "$data_dir/dhcpd.conf" -lf "$data_dir/dhcpd.leases" $IFACE
+    echo "Start dhcpd with $dhcpd_conf for interfaces: $IFACE"
+    exec /usr/sbin/dhcpd -$DHCPD_PROTOCOL -f -d --no-pid -cf "$dhcpd_conf" -lf "$data_dir/dhcpd.leases" $IFACE
 else
     # Run another binary
-    $run "$@"
+    exec "$@"
 fi
